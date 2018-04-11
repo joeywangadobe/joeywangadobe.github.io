8904c0a0ef4e845059746e75b30b4903b0d19f6d
Joey Wang <zhenwang@adobe.com>
2018-04-11 17:28:20 -0600

    run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 sudo yum install -y java-1.8.0-openjdk
+wget -O /jmeter.tgz https://rawgit.com/joeyzywang/azurejmeter/master/apache-jmeter-3.3.tgz
+tar zvxf /jmeter.tgz -C /
+mv /apache-jmeter-3.3/bin/jmeter /apache-jmeter-3.3/bin/jmeter_bkup
+sed 's/HEAP="-Xms512m -Xmx512m"/HEAP="-Xms8000m -Xmx10000m"/' bin/jmeter_bkup > bin/jmeter
+chmod 755 bin/jmeter
