f768f08b3b38fff3bfa4ea7fd28759fddb6a12d1
Joey Wang <zhenwang@adobe.com>
2018-04-27 14:27:27 -0600

    open jdk8

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 cd /home/azureuser
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie"  http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.rpm
-yum localinstall -y jdk-8u161-linux-x64.rpm
+yum install java-1.8.0-openjdk
 wget -O /home/azureuser/jmeter.tgz https://rawgit.com/joeyzywang/azurejmeter/master/apache-jmeter-3.3.tgz
 tar zvxf /home/azureuser/jmeter.tgz -C /home/azureuser/
 chown -R azureuser:azureuser /home/azureuser/apache-jmeter-3.3
 mv /home/azureuser/apache-jmeter-3.3/bin/jmeter /home/azureuser/apache-jmeter-3.3/bin/jmeter_bkup
-sed 's/HEAP="-Xms512m -Xmx512m"/HEAP="-Xms8000m -Xmx10000m"/' /home/azureuser/apache-jmeter-3.3/bin/jmeter_bkup > /home/azureuser/apache-jmeter-3.3/bin/jmeter
+sed 's/HEAP="-Xms512m -Xmx512m"/HEAP="-Xms8192m -Xmx10240m"/' /home/azureuser/apache-jmeter-3.3/bin/jmeter_bkup > /home/azureuser/apache-jmeter-3.3/bin/jmeter
 chmod 755 /home/azureuser/apache-jmeter-3.3/bin/jmeter
