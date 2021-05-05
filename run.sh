d0f0b7ee955f04b2a6266d9258f0620d3cdde033
Joey Wang <51214871+joeywangadobe@users.noreply.github.com>
2021-05-05 11:19:15 -0600

    Reduce Heap memory and change file owner

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@ wget -O /home/azureuser/jmeter.tgz https://joeywangadobe.github.io/azure-apache-
 tar vxf /home/azureuser/jmeter.tgz -C /home/azureuser/
 chown -R azureuser:azureuser /home/azureuser/apache-jmeter-3.3
 mv /home/azureuser/apache-jmeter-3.3/bin/jmeter /home/azureuser/apache-jmeter-3.3/bin/jmeter_bkup
-sed 's/HEAP="-Xms512m -Xmx512m"/HEAP="-Xms8192m -Xmx10240m"/' /home/azureuser/apache-jmeter-3.3/bin/jmeter_bkup > /home/azureuser/apache-jmeter-3.3/bin/jmeter
+sed 's/HEAP="-Xms512m -Xmx512m"/HEAP="-Xms1024m -Xmx10240m"/' /home/azureuser/apache-jmeter-3.3/bin/jmeter_bkup > /home/azureuser/apache-jmeter-3.3/bin/jmeter
 chmod 755 /home/azureuser/apache-jmeter-3.3/bin/jmeter
+chown azureuser:azureuser /home/azureuser/apache-jmeter-3.3/bin/jmeter
